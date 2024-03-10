b5078201728634fc0a592d8721c1993643b90d80
AntDavi <ant.davi07@gmail.com>
2024-03-10 18:49:55 -0300

    nova frase

--- containers/Phrase.jsx
+++ containers/Phrase.jsx
@@ -4,9 +4,9 @@ const Phrase = () => {
             <div className="container mx-auto">
                 <div className="flex flex-col items-center">
                     <h2 className="max-w-2xl mb-8 text-center h2 tracking-[4px]">
-                        Um problema bem formulado, é um problema meio resolvido
+                        Sou apaixonado por tecnologia, especialmente pela programação front-end e UX, onde posso unir a beleza do design digital com a interação humana, criando experiências incríveis para as pessoas.
                     </h2>
-                    <span className="text-primary">- Charles Kettering</span>
+                    {/* <span className="text-primary">- Charles Kettering</span> */}
                 </div>
             </div>
         </section>
