2456d1269daab71445e54475783b9b14185bb2ba
AntDavi <ant.davi07@gmail.com>
2024-03-13 07:19:01 -0300

    reponsividade blog

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -13,7 +13,7 @@ const PostCard = ({ post }) => {
         <Link
             href={`blog/${post.slug}`}
         >
-            <Card className='flex h-[250px] cursor-pointer overflow-hidden'>
+            <Card className='flex lg:h-[250px] cursor-pointer overflow-hidden lg:flex-row flex-col items-center'>
                 <CardHeader>
                     <Image
                         src={urlForImage(post.imgUrl)}
@@ -21,17 +21,17 @@ const PostCard = ({ post }) => {
                         height={200}
                         priority
                         alt={post.title}
-                        className='overflow-hidden rounded-sm'
+                        className='hidden rounded-sm lg:flex max-w-[200px] max-h-[200px]'
                     />
                 </CardHeader>
-                <CardContent className='w-full max-w-[700px] relative'>
+                <CardContent className='w-full max-w-[700px] flex flex-col items-center lg:items-start text-center lg:text-start'>
                     <h2>
                         {post.title}
                     </h2>
                     <p className='pt-2 pb-3'>
                         {post.excerpt}
                     </p>
-                    <div className='absolute bottom-6 '>
+                    <div className=''>
                         <span className='font-semibold bg-primary px-[8px] py-[4px] rounded-lg text-white'>
                             {post.tag}
                         </span>
